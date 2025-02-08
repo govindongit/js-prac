8c25be7bd3c8da9ef275bbf9b5544bf7a4b7b5db
govindongit <govindnag5863@gmail.com>
2025-02-09 00:01:02 +0530

    object

---
+++ object/object.js
@@ -0,0 +1,12 @@
+// Using es6 classes create objects in JavaScript
+class Vehicle {
+  constructor(name, maker, engine) {
+    this.name = name;
+    this.maker = maker;
+    this.engine = engine;
+  }
+}
+
+let car1 = new Vehicle("GT", "BMW", "1998cc");
+
+console.log(car1.name); //GT
